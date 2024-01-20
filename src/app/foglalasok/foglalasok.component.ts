30aad437201f75059a20589243879c3c8ea876df
Grünhut Gábor <grunhutg@gmail.com>
2024-01-20 13:59:27 +0100

    Foglalasok component_3

--- src/app/foglalasok/foglalasok.component.ts
+++ src/app/foglalasok/foglalasok.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FoglalasService } from '../foglalas.service';
 
 @Component({
   selector: 'app-foglalasok',
   templateUrl: './foglalasok.component.html',
-  styleUrls: ['./foglalasok.component.scss']
+  styleUrls: ['./foglalasok.component.css']
 })
-
 export class FoglalasokComponent implements OnInit {
   foglalasok: any[] = [];
 
-  constructor(private foglalasService: FoglalasService) {}
+  constructor(private foglalasService: FoglalasService, private router: Router) {}
 
   ngOnInit(): void {
     this.getFoglalasok();
@@ -19,7 +19,17 @@ export class FoglalasokComponent implements OnInit {
   getFoglalasok(): void {
     this.foglalasService
       .getFoglalasok()
-      .subscribe((foglalasok: any[]) => (this.foglalasok = foglalasok));
+      .subscribe((foglalasok: any[]) => {
+        this.foglalasok = foglalasok.sort((a, b) => new Date(b.datum).getTime() - new Date(a.datum).getTime());
+      });
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  navigateToUjFoglalas(): void {
+    this.router.navigate(['/ujfoglalas']);
   }
 }
 
